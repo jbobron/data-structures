4c43813e4e315d24cdbcb56dbaa6e651ff7d57fe
jbobron <jbobron@gmail.com>
2014-12-10 16:44:15 -0800

    through functional queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,59 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.counter = 0
+  return extend(obj, Queue.queueMethods);
 };
 
-var queueMethods = {};
+
+var extend = function(obj){
+  var args = Array.prototype.slice.call(arguments, 1);
+  for ( var i = 0; i < args.length; i++ ) {
+    var source = args[i];
+    for ( var key in source ) {
+      obj[key] = source[key];
+    }
+  }
+
+  return obj;
+};
+
+
+Queue.queueMethods = {};
+
+
+Queue.queueMethods.enqueue = function(value){
+  //obj[counter] = value;
+
+};
+
+Queue.queueMethods.dequeue = function() {
+
+
+};
+
+
+Queue.queueMethods.size = function() {
+  return this.counter;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function(){
+    //delete storage[0];
+    if(counter > 0){
+      var value = storage[0];
+      counter--;
+      for(var i = counter; i > 0; i--){
+        storage[i-1] = storage[i];
+      }
+
+      delete storage[counter];
+      return value;
+    }
+
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
