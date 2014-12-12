bdb05468dddf2d9d146a1f590062c482fc6692d6
jbobron <jbobron@gmail.com>
2014-12-11 21:18:57 -0800

    done with linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,19 +4,60 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    //if list.head && list.tail === null
     var node = Node(value);
-    list.tail = node;
+
+    if ( list.head === null && list.tail === null ) {
+      // question: does list.tail and list.head have a unique copy of node? or are they pointing to the same object in memory??
+      list.tail = node;
+      list.head = node;
+    } else {
+
+      list.tail.next = node;
+
+      list.tail = node;
+      //list.head.next = list.tail;
+    }
 
   };
 
   list.removeHead = function(){
-    //
-    //
+    var temp = list.head.value;
+    if(list.head === list.tail && list.head !== null){
+      list.tail = null;
+      list.head = null;
+      return temp;
+    }else{
+      list.head = list.head.next;
+      return temp;
+    }
+
   };
 
   list.contains = function(target){
+    var results = false;
+      //while list.head.next != null
+      //INFINATE LOOOOOOP!
+
+      var traverseNodes = function(node){
+
+          //console.log(node.value, target);
+          if(node.value === target){
+            results = true;
+            //console.log(results);
+            return results;
+          }
+          else{
+            console.log(node.next);
+            if(node.next){
+              traverseNodes(node.next);
+            }
+          }
+        return results;
+      }
+      return traverseNodes(list.head);
   };
+
+
 
   return list;
 };
