1dc65867964233ca8578ff69aa458e03c8433b90
jbobron <jbobron@gmail.com>
2014-12-13 18:19:21 -0800

    finished graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,12 +1,10 @@
 
 
 var Graph = function(){
-  var nodes = {};
-
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
-
 
   var obj = {};
   obj.node = node;
@@ -16,77 +14,38 @@ Graph.prototype.addNode = function(node){
 };
 
 Graph.prototype.contains = function(node){
-  var lookup = ""+node;
-  return this[lookup] ? true : false;
+  return this.nodes[node] ? true: false;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
-
-
-
-
-  // var fromLookup = ""+fromNode;
-  // var toLookup = ""+toNode;
-  // if(this.contains(fromLookup) && this.contains(toLookup)){
-  //   // console.log('this[toLookup].edges: ', this[toLookup].edges);
-  //   if(this[fromLookup].edges.length > 0) {
-  //     //_.each(this[fromLookup].edges, function(v, i){
-  //     for(var i = 0; i < this[fromLookup].edges.length; i++){
-  //       console.log('this[toLookup].edges[i]', this[toLookup].edges[i]);
-  //       console.log('this[fromLookup]', this[fromLookup]);
-  //       if( this[fromLookup].edges[i] ) {
-
-  //         return true;
-  //       }
-  //     }
-  //     //});
-  //   }
-  //   return false;
-  // }
-
+  if ( this.contains(fromNode) && this.contains(toNode) ) {
+    if ( this.nodes[fromNode].edges[toNode]  ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-  // //console.log("outer this: ", this);
-  // var fromLookup = ""+fromNode;
-  // var toLookup = ""+toNode;
-  // if(this.contains(fromLookup) && this.contains(toLookup)){
-  //   //console.log(this);
-  //   this[fromLookup].edges.push(this[toLookup]);
-  //   this[toLookup].edges.push(this[fromLookup]);
-  //   console.log(this[fromLookup]);
-
-  // }
+  this.nodes[fromNode].edges[toNode] = this.nodes[toNode];
+  this.nodes[toNode].edges[fromNode] = this.nodes[fromNode];
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
-
-  for (var key in this) {
-      cb(this[key]);
-
+  for (var node in this.nodes) {
+    cb(node);
   }
-  // var keyz = Object.keys(this);
-  // var thiz = this;
-  // _.each(keyz, function(key){
-  //   cb(thiz[key].node);
-  // });
-
-  // var thiz = this;
-  // var nodes = keyz.map(function(value){
-  //   return thiz[value];
-  // });
-
-  // console.log(keyz);
-  // keyz.forEach(cb);
-
 };
 
 /*
