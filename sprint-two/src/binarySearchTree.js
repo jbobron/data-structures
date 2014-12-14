0717e052828a0b32f096584b1469a5e61ff9588c
jbobron <jbobron@gmail.com>
2014-12-13 19:59:05 -0800

    started insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,33 @@
 var BinarySearchTree = function(value){
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  tree.insert = function(value){
+    var myTree = new BinarySearchTree(value);
+    if(value < this.value){
+      if(this.left === null){
+        this.left = myTree;
+      }else{
+        this.left.insert(value);
+      }
+    }else if(value > this.value){
+      if(this.right === null){
+        this.right = myTree;
+      }else{
+        this.right.insert(value);
+      }
+    }
+  }
+
+  return tree;
 };
+
+
+
+
+
 
 
 /*
